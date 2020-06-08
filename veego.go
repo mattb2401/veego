fdb09231546cf64305858b9c455c22874fafef8d
mathias sebuuma <mathias240@live.com>
2020-06-08 12:23:44 +0300

    remove app config dependency on server

--- veego.go
+++ veego.go
@@ -14,19 +14,21 @@ import (
 )
 
 type server struct {
-	config     *AppConfig
 	baseRouter *mux.Router
 }
 
-func NewServer(config *AppConfig, baseRouter *mux.Router) *server {
-	server := &server{
-		config:     config,
+var (
+	host = "0.0.0.0"
+	port = "8080" 
+)
+
+func NewServer(baseRouter *mux.Router) *server {
+	return &server{
 		baseRouter: baseRouter,
 	}
-	return server
 }
 
-func (s *server) Run() error {
+func (s *server) Run(args ...interface{}) error {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	ctx, cancel := context.WithCancel(context.Background())
@@ -35,8 +37,9 @@ func (s *server) Run() error {
 		fmt.Printf("%v", oscall)
 		cancel()
 	}()
+	if len(args) > 0 { host = args[0].(string); port = args[1].(string) }
 	server := &http.Server{
-		Addr:         fmt.Sprintf("%s:%s", s.config.Host, s.config.Port),
+		Addr:         fmt.Sprintf("%s:%s", host, port),
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
 		IdleTimeout:  time.Second * 60,
@@ -49,7 +52,7 @@ func (s *server) Run() error {
 			log.Fatalf("serve failed: %v \n", err.Error())
 		}
 	}()
-	fmt.Printf("Appliction running on %s:%s...\n", s.config.Host, s.config.Port)
+	fmt.Printf("Appliction running on %s:%s...\n", host, port)
 	<-ctx.Done()
 	fmt.Printf("Appliction stopped \n")
 	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
